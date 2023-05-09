386eaea61aefc823c895ebfc6a0cc21b290f6bf8
Peter Flimmel <peter.flimmel@isdd.sk>
2023-05-09 15:02:02 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './app.scss';
+
+const App: React.FC = () => {
+
+  return (
+    <>
+    <button className="govuk-button">This is a button component</button>
+    Hello
+    </>
+  );
+};
+
+export default App;
