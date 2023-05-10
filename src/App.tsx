d0f5f2c01e72f721539eff4142924735471c4771
Matesko7 <39712954+Matesko7@users.noreply.github.com>
2023-05-10 14:25:09 +0200

    layout + base components

--- src/App.tsx
+++ src/App.tsx
@@ -10,37 +10,39 @@ const App: React.FC = () => {
   const [name, setName] = useState<string>('Janko')
   const [lastName, setLastName] = useState<string>('Hraško')
   return (
-    <div style={{ maxWidth: 1600, margin: "auto" }}>
+    <>
       <Navbar />
-      <div style={{ marginTop: 16 }}>
-        <Table />
-      </div>
-      <div style={{ width: "60%", margin: "auto" }}>
-        <form action="/" method="post">
-          <Input
-            name="name"
-            value={name}
-            label="Meno použivateľa"
-            onChange={(value) => setName(value)}
-          />
-          <Input
-            name="lastName"
-            value={lastName}
-            label="Priezvisko použivateľa"
-            onChange={(value) => setLastName(value)}
-          />
-          <RadioButton
-            name="account"
-            value="Hraško"
-            label="Máte už vytvorený osobný účet?"
-          />
-          <div style={{ textAlign: "center" }}>
-            <button className="govuk-button">Odoslať formulár</button>
+      <div className="govuk-width-container" id="main-content">
+        <main className="govuk-main-wrapper govuk-main-wrapper--auto-spacing">
+          <Table />
+          <div>
+            <form action="/" method="post">
+              <Input
+                name="name"
+                value={name}
+                label="Meno použivateľa"
+                onChange={(value) => setName(value)}
+              />
+              <Input
+                name="lastName"
+                value={lastName}
+                label="Priezvisko použivateľa"
+                onChange={(value) => setLastName(value)}
+              />
+              <RadioButton
+                name="account"
+                value="Hraško"
+                label="Máte už vytvorený osobný účet?"
+              />
+              <div style={{ textAlign: "center" }}>
+                <button className="govuk-button">Odoslať formulár</button>
+              </div>
+            </form>
           </div>
-        </form>
+        </main>
       </div>
       <Footer />
-    </div>
+    </>
   );
 };
 
