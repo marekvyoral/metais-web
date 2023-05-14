b4993eeae8d2ef0ea1b1d738b403fbecceaf6a01
Matesko7 <39712954+Matesko7@users.noreply.github.com>
2023-05-14 18:00:40 +0200

    prettier config

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+	semi: true,
+	trailingComma: 'all',
+	singleQuote: true,
+	printWidth: 150,
+	tabWidth: 4,
+	useTabs: false,
+	bracketSpacing: true,
+	arrowParens: 'always',
+	endOfLine: 'lf',
+};
