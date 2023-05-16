9aa7cdc75ff1dee1ef2e0670d6cfeb57255c1be9
MIROSLAV VANO <miroslav13vano@gmail.com>
2023-05-16 14:09:01 +0200

    eslint, prettier changes

--- src/components/grid/gridCol.tsx
+++ src/components/grid/gridCol.tsx
@@ -1,13 +1,10 @@
-import React, { ReactElement, useMemo, useState } from 'react';
+import React, { ReactElement } from 'react'
 
 interface IGridColProps {
-    children: ReactElement;
-    setWidth: 'full' | 'two-thirds' | 'one-half' | 'one-third' | 'one-quarter';
+  children: ReactElement
+  setWidth: 'full' | 'two-thirds' | 'one-half' | 'one-third' | 'one-quarter'
 }
 
-const GridCol = ({ children, setWidth }: IGridColProps) => {
-    return <div className={`govuk-grid-column-${setWidth}`}>{children}</div>;
-};
-
-export default GridCol;
-
+export const GridCol = ({ children, setWidth }: IGridColProps) => {
+  return <div className={`govuk-grid-column-${setWidth}`}>{children}</div>
+}
--- src/components/grid/gridRow.tsx
+++ src/components/grid/gridRow.tsx
@@ -1,11 +1,9 @@
-import { ReactElement, ReactNode } from 'react';
+import React, { ReactElement } from 'react'
 
 interface IGridRowProps {
-    children: ReactElement[] | ReactElement;
+  children: ReactElement[] | ReactElement
 }
 
-const GridRow = ({ children }: IGridRowProps) => {
-    return <div className="govuk-grid-row">{children}</div>;
-};
-
-export default GridRow;
+export const GridRow = ({ children }: IGridRowProps) => {
+  return <div className="govuk-grid-row">{children}</div>
+}
