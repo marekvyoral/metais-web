0687b6297185001fa5c60a215624c8b73d0cf1c2
Takacs Tomas <tomas.takacs@isdd.sk>
2023-05-17 14:08:17 +0000

    Setup/storybook init

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-// eslint-disable-next-line import/no-default-export
 export default defineConfig({
   plugins: [react()],
   server: {
