461fdfeb306d518ce7b66630da833243c6006db0
Svec Matej <matej.svec@isdd.sk>
2023-05-18 08:48:17 +0000

    react route dom

--- src/components/bread-crumbs/BreadCrumbs.tsx
+++ src/components/bread-crumbs/BreadCrumbs.tsx
@@ -2,34 +2,34 @@ import * as React from 'react'
 import './breadCrumbs.scss'
 
 type BreadCrumbsItemProps = {
-  icon?: string
-  href: string
-  label: string
+    icon?: string
+    href: string
+    label: string
 }
 
 type BreadCrumbsProps = {
-  links: BreadCrumbsItemProps[]
+    links: BreadCrumbsItemProps[]
 }
 
 const BreadCrumbsItem: React.FC<BreadCrumbsItemProps> = ({ icon, href, label }) => {
-  return (
-    <li className="govuk-breadcrumbs__list-item">
-      <a className="govuk-breadcrumbs__link" href={href}>
-        {icon && <img src={icon} alt="icon" />}
-        {label}
-      </a>
-    </li>
-  )
+    return (
+        <li className="govuk-breadcrumbs__list-item">
+            <a className="govuk-breadcrumbs__link" href={href}>
+                {icon && <img src={icon} alt="icon" />}
+                {label}
+            </a>
+        </li>
+    )
 }
 
 export const BreadCrumbs: React.FC<BreadCrumbsProps> = ({ links }) => {
-  return (
-    <div className="govuk-breadcrumbs">
-      <ul className="govuk-breadcrumbs__list">
-        {links.map((value) => (
-          <BreadCrumbsItem href={value.href} label={value.label} icon={value.icon} key={value.label} />
-        ))}
-      </ul>
-    </div>
-  )
+    return (
+        <div className="govuk-breadcrumbs">
+            <ul className="govuk-breadcrumbs__list">
+                {links.map((value) => (
+                    <BreadCrumbsItem href={value.href} label={value.label} icon={value.icon} key={value.label} />
+                ))}
+            </ul>
+        </div>
+    )
 }
