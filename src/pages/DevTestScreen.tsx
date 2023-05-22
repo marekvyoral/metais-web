82113aa8c3df0cb413e52305c97c88b1b17b5116
Mariia <mariia.naduieva@isdd.sk>
2023-05-22 11:04:12 +0200

    fix error

--- src/pages/DevTestScreen.tsx
+++ src/pages/DevTestScreen.tsx
@@ -1,36 +1,11 @@
 import * as React from 'react'
-import { SubmitHandler, useForm } from 'react-hook-form'
-
-import { RadioButton } from '../components/RadioButton'
-
-interface IFormInput {
-    name: string | undefined
-}
 
 import { AccordionContainer } from '../components/Accordion'
 
 export const DevTestScreen: React.FC = () => {
-    const { register, handleSubmit } = useForm({
-        defaultValues: {
-            name: undefined,
-        },
-    })
-
-    const onSubmit: SubmitHandler<IFormInput> = (data) => console.log(data)
     return (
         <>
             <h4>Obrazovka na testovanie komponentov</h4>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <fieldset className="govuk-fieldset">
-                    <div className="govuk-radios">
-                        <RadioButton {...register('name')} id="id" name="name" value="ano" label="ano" />
-                        <RadioButton {...register('name')} id="id1" name="name" value="nie" label="nie" />
-                    </div>
-                </fieldset>
-                <div>
-                    <button className="govuk-button">Odoslať formulár</button>
-                </div>
-            </form>
 
             <AccordionContainer
                 sections={[
