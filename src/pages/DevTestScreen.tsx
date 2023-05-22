0f7e9c4bf1b5fb7853f0a15253bba1db9e141055
Mariia <mariia.naduieva@isdd.sk>
2023-05-22 10:55:16 +0200

    fix radiobutton

--- src/pages/DevTestScreen.tsx
+++ src/pages/DevTestScreen.tsx
@@ -4,8 +4,7 @@ import { SubmitHandler, useForm } from 'react-hook-form'
 import { RadioButton } from '../components/RadioButton'
 
 interface IFormInput {
-    name: string
-    name1: string
+    name: string | undefined
 }
 
 import { AccordionContainer } from '../components/Accordion'
@@ -13,8 +12,7 @@ import { AccordionContainer } from '../components/Accordion'
 export const DevTestScreen: React.FC = () => {
     const { register, handleSubmit } = useForm({
         defaultValues: {
-            name: '',
-            name1: '',
+            name: undefined,
         },
     })
 
@@ -24,8 +22,10 @@ export const DevTestScreen: React.FC = () => {
             <h4>Obrazovka na testovanie komponentov</h4>
             <form onSubmit={handleSubmit(onSubmit)}>
                 <fieldset className="govuk-fieldset">
-                    <RadioButton {...register('name')} id="id" name="name" value="Hraško" label="ano" />
-                    <RadioButton {...register('name1')} id="id1" name="name" value="Hraško1" label="ano1" />
+                    <div className="govuk-radios">
+                        <RadioButton {...register('name')} id="id" name="name" value="ano" label="ano" />
+                        <RadioButton {...register('name')} id="id1" name="name" value="nie" label="nie" />
+                    </div>
                 </fieldset>
                 <div>
                     <button className="govuk-button">Odoslať formulár</button>
