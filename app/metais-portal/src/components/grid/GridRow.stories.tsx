f50231a055e2aac5de0257bbf40a0a31c031bd4a
Matej Svec <matej.svec@dynatech.sk>
2023-05-29 15:09:52 +0200

    moved files from src to app/metais-portal

---
+++ app/metais-portal/src/components/grid/GridRow.stories.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { GridRow } from '@/components/grid/GridRow'
+
+const meta: Meta<typeof GridRow> = {
+    title: 'Components/GridRow',
+    component: GridRow,
+}
+export default meta
+
+type Story = StoryObj<typeof GridRow>
+
+export const Default: Story = {
+    render: () => (
+        <GridRow>
+            <div style={{ backgroundColor: 'blue', height: 20, width: '100%' }} />
+        </GridRow>
+    ),
+}
