21c0232db1370fcded8d46f8b3e774608648cb41
Sebastián Simko <contact@sebastiansimko.sk>
2023-05-31 09:35:38 +0200

    eslint

--- app/metais-admin/src/index.tsx
+++ app/metais-admin/src/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { BrowserRouter } from 'react-router-dom'
 import { I18nextProvider } from 'react-i18next'
@@ -9,20 +9,19 @@ import { reportWebVitals } from '@/reportWebVitals'
 import { i18nInstance } from '@/localization/i18next'
 import './index.scss'
 
-
+const root = createRoot(document.getElementById('root') as HTMLElement)
 const queryClient = new QueryClient()
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <QueryClientProvider client={queryClient}>
-        <I18nextProvider i18n={i18nInstance}>
-          <App />
-        </I18nextProvider>
-      </QueryClientProvider>
-    </BrowserRouter>
-  </React.StrictMode>,
+root.createRoot(document.getElementById('root') as HTMLElement).render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <QueryClientProvider client={queryClient}>
+                <I18nextProvider i18n={i18nInstance}>
+                    <App />
+                </I18nextProvider>
+            </QueryClientProvider>
+        </BrowserRouter>
+    </React.StrictMode>,
 )
-
 
 reportWebVitals()
--- app/metais-admin/src/pages/DevTestScreen.tsx
+++ app/metais-admin/src/pages/DevTestScreen.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 
-
 export const DevTestScreen: React.FC = () => {
     return (
         <>
