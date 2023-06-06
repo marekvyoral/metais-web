3c7d4bdb3940cdc1adda2537d303d7db2dbd3367
martin.bilansky <martin.bilansky@iteria.io>
2023-06-06 09:46:45 +0200

    fix(duplicity): duplicity in generated code

--- orval.config.ts
+++ orval.config.ts
@@ -25,6 +25,27 @@ export default defineConfig({
     cmdbSwagger: {
         input: {
             target: process.env.VITE_CMDB_SWAGGER_API_ENDPOINT ?? '',
+            filters: {
+                tags: [
+                    'cmdb-cache-controller',
+                    'cmdb-change-owner-controller',
+                    'cmdb-elastic-delete-controller',
+                    'cmdb-elastic-read-controller',
+                    'cmdb-elastic-store-controller',
+                    'cmdb-history-read-controller',
+                    'cmdb-invalidate-controller',
+                    'cmdb-metrics-controller',
+                    'cmdb-po-controller',
+                    'cmdb-read-controller',
+                    'cmdb-recycle-controller',
+                    'cmdb-request-tracking-controller',
+                    'cmdb-rights-controller',
+                    'cmdb-store-controller',
+                    'cmdb-user-feedback-controller',
+                    'cmdb-util-controller',
+                    'health-controller',
+                ],
+            },
         },
         output: {
             target: `./app/metais-portal/src/api/generated/cmdb-swagger.ts`,
@@ -37,6 +58,17 @@ export default defineConfig({
     typesRepo: {
         input: {
             target: process.env.VITE_TYPES_REPO_SWAGGER_API_ENDPOINT ?? '',
+            filters: {
+                tags: [
+                    'attribute-controller',
+                    'attribute-profile-controller',
+                    'ci-type-controller',
+                    'ci-type-relationship-type-map-controller',
+                    // 'health-controller',
+                    'relationship-type-controller',
+                    'rights-type-controller',
+                ],
+            },
         },
         output: {
             target: `./app/metais-portal/src/api/generated/types-repo-swagger.ts`,
