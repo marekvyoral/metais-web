527d77866e1155144509d8f9aef1923432cfb883
Sebastián Simko <contact@sebastiansimko.sk>
2023-06-23 12:38:59 +0200

    MET-64: add translations

--- packages/idsk-ui-kit/filter/Filter.tsx
+++ packages/idsk-ui-kit/filter/Filter.tsx
@@ -1,4 +1,6 @@
+import classNames from 'classnames'
 import React, { FC, useState } from 'react'
+import {useTranslation} from "react-i18next";
 
 type FilterProps = {
     heading: React.ReactNode
@@ -7,23 +9,24 @@ type FilterProps = {
 }
 
 export const Filter: FC<FilterProps> = ({ form, heading, onSubmit }) => {
+    const { t } = useTranslation()
     const [isOpen, setOpen] = useState(false)
     return (
         <div data-module="idsk-table-filter" id="example-table-2-filter" className="idsk-table-filter">
-            <div className={`idsk-table-filter__panel idsk-table-filter__inputs ${isOpen ? 'idsk-table-filter--expanded' : ''}`}>
+            <div className={classNames('idsk-table-filter__panel idsk-table-filter__inputs', { 'idsk-table-filter--expanded': isOpen })}>
                 <div className="idsk-table-filter__title govuk-heading-m">{heading}</div>
                 <button
                     onClick={() => setOpen(!isOpen)}
                     className="govuk-body govuk-link idsk-filter-menu__toggle"
                     tabIndex={0}
                     data-category-name=""
-                    aria-label={isOpen ? 'Zbaliť obsah filtra' : 'Rozbaliť obsah filtra'}
+                    aria-label={isOpen ? t('filter.collapse') : t('filter.expand')}
                     type="button"
                 >
-                    {isOpen ? 'Zbaliť obsah filtra' : 'Rozbaliť obsah filtra'}
+                    {isOpen ? t('filter.collapse') : t('filter.expand')}
                 </button>
 
-                <form className={`idsk-table-filter__content${isOpen ? '--expanded' : ''}`} action="#" onSubmit={(e) => onSubmit(e)}>
+                <form className={classNames('idsk-table-filter__content', { 'idsk-table-filter__content--expanded': isOpen })} action="#" onSubmit={(e) => onSubmit(e)}>
                     {form}
                 </form>
             </div>
