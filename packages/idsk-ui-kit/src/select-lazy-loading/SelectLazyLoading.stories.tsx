4e35fb267c333c5394c7b99759c7a8eea74f0223
Mihalik Oskar <oskar.mihalik@iteria.io>
2023-07-13 11:19:09 +0000

    MET-341-multi-select

--- packages/idsk-ui-kit/src/select-lazy-loading/SelectLazyLoading.stories.tsx
+++ packages/idsk-ui-kit/src/select-lazy-loading/SelectLazyLoading.stories.tsx
@@ -22,7 +22,7 @@ export const Minimal: Story = {
         getOptionLabel: (item) => item.name,
         option: undefined,
         loadOptions: async () => ({
-            options: [{ name: 'Item Name 123' }],
+            options: [{ name: 'Item Name 123' }, { name: 'zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz' }],
             hasMore: false,
             additional: {
                 page: 0,
