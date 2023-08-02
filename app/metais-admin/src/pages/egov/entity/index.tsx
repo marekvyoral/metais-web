1989b71b2509143649a9f178cb5449496a47cdde
Oleksandr Panyshchev <oleksandr.panyshchev@isdd.sk>
2023-08-02 15:30:18 +0200

    ts check

--- app/metais-admin/src/pages/egov/entity/index.tsx
+++ app/metais-admin/src/pages/egov/entity/index.tsx
@@ -1,16 +1,11 @@
 import React from 'react'
 
-<<<<<<<< HEAD:app/metais-admin/src/pages/egov/entity/index.tsx
 import Entity from './entity'
 
 export const INDEX_ROUTE = Entity
 
-const index: React.FC = () => {
-    return <Outlet />
-========
 const index = () => {
     return <div>index</div>
->>>>>>>> develop:app/metais-admin/src/pages/egov/index.tsx
 }
 
 export default index
