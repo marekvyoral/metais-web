07fc4a6aba708f60d849de48298d0b69e6faab3b
Badura Filip <filip.badura@isdd.sk>
2023-08-10 17:30:33 +0000

    Feature/met 54 tasks

--- packages/metais-common/src/api/hooks/useIamSwaggerClient.tsx
+++ packages/metais-common/src/api/hooks/useIamSwaggerClient.tsx
@@ -1,3 +1,3 @@
 import { useCustomClient } from './use-custom-client'
 const baseURL = import.meta.env.VITE_REST_CLIENT_IAM_TARGET_URL
-export const useIAmSwaggerClient = <T>() => useCustomClient<T>(baseURL)
+export const useIAmSwaggerClient = <T,>() => useCustomClient<T>(baseURL)
