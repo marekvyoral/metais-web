87cccfd590a9fc6729b74fb3f77f5f07f6dcbfcc
Hryshchuk Mykola <mykola.hryshchuk@isdd.sk>
2023-08-30 15:11:14 +0200

    MET-429: remove unused files

--- app/metais-portal/src/components/ci-table/ciTableFormatter.ts
+++
@@ -1,7 +0,0 @@
-import { formatToDateTime, isValidDate } from '@isdd/metais-common/index'
-
-export const formatLabelByAttribute = (value: string, defaultString: string) => {
-    if (!value) return ''
-    if (isValidDate(value)) return formatToDateTime(value)
-    return defaultString
-}
