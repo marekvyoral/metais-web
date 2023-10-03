f5d096762609fe624c9a5508d96befb0bb3ef2d9
Vano Miroslav <miroslav.vano@isdd.sk>
2023-10-03 09:57:17 +0000

    MET-545: admin routes permissions

--- app/metais-admin/src/components/Navbar.tsx
+++ app/metais-admin/src/components/Navbar.tsx
@@ -2,96 +2,152 @@ import { InstructionsIcon } from '@isdd/metais-common/assets/images'
 import { NavBarHeader } from '@isdd/metais-common/components/navbar/navbar-header/NavBarHeader'
 import { NavBarMain } from '@isdd/metais-common/components/navbar/navbar-main/NavBarMain'
 import { NavMenu } from '@isdd/metais-common/components/navbar/navmenu/NavMenu'
-import { AdminRouteNames, NavigationItem } from '@isdd/metais-common/navigation/routeNames'
+import { useAuth } from '@isdd/metais-common/contexts/auth/authContext'
+import { AdminRouteNames, AdminRouteRoles, NavigationItem } from '@isdd/metais-common/navigation/routeNames'
 import { TFunction } from 'i18next'
 import React, { useState } from 'react'
 import { useTranslation } from 'react-i18next'
 
-export const getAdminNavItems = (t: TFunction): NavigationItem[] => [
-    {
-        title: t('navMenu.userManagement.userManagement'),
-        path: AdminRouteNames.USER_MANAGEMENT,
-        icon: InstructionsIcon,
-        subItems: [
-            {
-                title: t('navMenu.userManagement.users'),
-                path: AdminRouteNames.USERS,
-            },
-            {
-                title: t('navMenu.userManagement.requestList'),
-                path: AdminRouteNames.REQUEST_LIST_ALL,
-            },
-        ],
-    },
-    {
-        title: t('navMenu.roleManagement'),
-        path: AdminRouteNames.ROLES,
-        icon: InstructionsIcon,
-    },
-    {
-        title: t('navMenu.publicAuthorities.management'),
-        path: '/public-authorities',
-        icon: InstructionsIcon,
-        subItems: [
-            {
-                title: t('navMenu.publicAuthorities.publicAuthorities'),
-                path: AdminRouteNames.ORGANIZATIONS,
-            },
-            { title: t('navMenu.publicAuthorities.massActualizations'), path: '/public-authorities/mass-updates' },
-        ],
-    },
-    {
-        title: t('navMenu.egov.egov'),
-        path: AdminRouteNames.EGOV,
-        icon: InstructionsIcon,
-        subItems: [
-            {
-                title: t('navMenu.egov.entity'),
-                path: AdminRouteNames.EGOV_ENTITY,
-            },
-            {
-                title: t('navMenu.egov.profiles'),
-                path: AdminRouteNames.EGOV_PROFILE,
-            },
-            {
-                title: t('navMenu.egov.relations'),
-                path: AdminRouteNames.EGOV_RELATION,
-            },
-        ],
-    },
-    {
-        title: t('navMenu.codelists'),
-        path: AdminRouteNames.CODELISTS,
-        icon: InstructionsIcon,
-    },
-    {
-        title: t('navMenu.monitoring.monitoring'),
-        //in admin is /list but that is not unique
-        path: AdminRouteNames.MONITORING,
-        icon: InstructionsIcon,
-        subItems: [
-            //did not find similar routes in admin so I made custom ones
-            { title: t('navMenu.monitoring.params'), path: AdminRouteNames.MONITORING_PARAMS },
-            { title: t('navMenu.monitoring.list'), path: AdminRouteNames.MONITORING_LIST },
-        ],
-    },
-    //predpokladam ze to je sprava zostav
-    { title: t('navMenu.reportsManagement'), path: AdminRouteNames.REPORTS_MANAGEMENT, icon: InstructionsIcon },
-    {
-        title: t('navMenu.projects.management'),
-        path: AdminRouteNames.PROJECTS_MANAGEMENT,
-        icon: InstructionsIcon,
-        subItems: [
-            { title: t('navMenu.projects.financeManagement'), path: AdminRouteNames.PROJECTS_FINANCE_MANAGEMENT },
-            {
-                title: t('navMenu.eko'),
-                path: AdminRouteNames.EKO,
-            },
-            { title: t('navMenu.documentsManagement'), path: AdminRouteNames.DOCUMENTS_MANAGEMENT },
-        ],
-    },
-    { title: t('navMenu.templateReferenceIdentifiersManagement'), path: AdminRouteNames.TEMPLATE_REFERENCE_IDENTIFIERS, icon: InstructionsIcon },
-]
+import { getPermittedRoutesForUser } from '@/componentHelpers/navigation'
+import { useAdminProtectedRoutes } from '@/hooks/permissions/useAdminProtectedRoutes'
+
+export const getAdminNavItems = (t: TFunction, userRoles: string[] | null): NavigationItem[] => {
+    const adminRoutes: NavigationItem[] = [
+        {
+            title: t('navMenu.userManagement.userManagement'),
+            path: AdminRouteNames.USER_MANAGEMENT,
+            icon: InstructionsIcon,
+            role: [AdminRouteRoles.ADMIN, AdminRouteRoles.HKO],
+            subItems: [
+                {
+                    title: t('navMenu.userManagement.users'),
+                    role: [AdminRouteRoles.ADMIN, AdminRouteRoles.HKO],
+                    path: AdminRouteNames.USERS,
+                },
+                {
+                    title: t('navMenu.userManagement.requestList'),
+                    role: [AdminRouteRoles.ADMIN, AdminRouteRoles.HKO],
+                    path: AdminRouteNames.REQUEST_LIST_ALL,
+                },
+            ],
+        },
+        {
+            title: t('navMenu.roleManagement'),
+            path: AdminRouteNames.ROLES,
+            icon: InstructionsIcon,
+            role: [AdminRouteRoles.ADMIN],
+        },
+        {
+            title: t('navMenu.publicAuthorities.management'),
+            path: '/public-authorities',
+            icon: InstructionsIcon,
+            role: [AdminRouteRoles.ADMIN],
+            subItems: [
+                {
+                    title: t('navMenu.publicAuthorities.publicAuthorities'),
+                    role: [AdminRouteRoles.ADMIN],
+                    path: AdminRouteNames.ORGANIZATIONS,
+                },
+                {
+                    title: t('navMenu.publicAuthorities.massActualizations'),
+                    path: '/public-authorities/mass-updates',
+                    role: [AdminRouteRoles.ADMIN],
+                },
+            ],
+        },
+        {
+            title: t('navMenu.egov.egov'),
+            path: AdminRouteNames.EGOV,
+            icon: InstructionsIcon,
+            role: [AdminRouteRoles.ADMIN],
+            subItems: [
+                {
+                    title: t('navMenu.egov.entity'),
+                    path: AdminRouteNames.EGOV_ENTITY,
+                    role: [AdminRouteRoles.ADMIN],
+                },
+                {
+                    title: t('navMenu.egov.profiles'),
+                    path: AdminRouteNames.EGOV_PROFILE,
+                    role: [AdminRouteRoles.ADMIN],
+                },
+                {
+                    title: t('navMenu.egov.relations'),
+                    path: AdminRouteNames.EGOV_RELATION,
+                    role: [AdminRouteRoles.ADMIN],
+                },
+            ],
+        },
+        {
+            title: t('navMenu.codelists'),
+            path: AdminRouteNames.CODELISTS,
+            icon: InstructionsIcon,
+            role: [AdminRouteRoles.ADMIN],
+        },
+        {
+            title: t('navMenu.monitoring.monitoring'),
+            //in admin is /list but that is not unique
+            path: AdminRouteNames.MONITORING,
+            icon: InstructionsIcon,
+            role: [AdminRouteRoles.ADMIN, AdminRouteRoles.HKO],
+            subItems: [
+                //did not find similar routes in admin so I made custom ones
+                {
+                    title: t('navMenu.monitoring.params'),
+                    path: AdminRouteNames.MONITORING_PARAMS,
+                    role: [AdminRouteRoles.ADMIN, AdminRouteRoles.HKO],
+                },
+                {
+                    title: t('navMenu.monitoring.list'),
+                    path: AdminRouteNames.MONITORING_LIST,
+                    role: [AdminRouteRoles.ADMIN, AdminRouteRoles.HKO],
+                },
+            ],
+        },
+        //predpokladam ze to je sprava zostav
+        {
+            title: t('navMenu.reportsManagement'),
+            path: AdminRouteNames.REPORTS_MANAGEMENT,
+            icon: InstructionsIcon,
+            role: [AdminRouteRoles.ADMIN, AdminRouteRoles.METAIS],
+        },
+        {
+            title: t('navMenu.projects.management'),
+            path: AdminRouteNames.PROJECTS_MANAGEMENT,
+            icon: InstructionsIcon,
+            role: [AdminRouteRoles.ADMIN, AdminRouteRoles.METAIS],
+            subItems: [
+                {
+                    title: t('navMenu.projects.financeManagement'),
+                    path: AdminRouteNames.PROJECTS_FINANCE_MANAGEMENT,
+                    role: [AdminRouteRoles.ADMIN],
+                },
+                {
+                    title: t('navMenu.eko'),
+                    path: AdminRouteNames.EKO,
+                    //not sure about this TODO
+                    role: [AdminRouteRoles.ADMIN],
+                },
+                {
+                    title: t('navMenu.documentsManagement'),
+                    path: AdminRouteNames.DOCUMENTS_MANAGEMENT,
+                    role: [AdminRouteRoles.ADMIN, AdminRouteRoles.METAIS],
+                },
+            ],
+        },
+        //not sure about this TODO
+        {
+            title: t('navMenu.templateReferenceIdentifiersManagement'),
+            path: AdminRouteNames.TEMPLATE_REFERENCE_IDENTIFIERS,
+            icon: InstructionsIcon,
+            role: [AdminRouteRoles.ADMIN],
+        },
+    ]
+
+    if (userRoles == null || userRoles.length == 0) return []
+
+    return getPermittedRoutesForUser(adminRoutes, userRoles)
+}
 
 type NavbarProps = {
     isAdmin?: boolean
@@ -99,8 +155,14 @@ type NavbarProps = {
 
 export const Navbar: React.FC<NavbarProps> = ({ isAdmin }) => {
     const { t } = useTranslation()
+    const {
+        state: { user },
+    } = useAuth()
     const [isMenuExpanded, setIsMenuExpanded] = useState<boolean>(false)
     const [showDropDown, setShowDropDown] = useState<boolean>(false)
+
+    const adminRoutes = getAdminNavItems(t, user?.roles ?? null)
+    useAdminProtectedRoutes(adminRoutes)
 
     return (
         <>
@@ -113,7 +175,7 @@ export const Navbar: React.FC<NavbarProps> = ({ isAdmin }) => {
 
                     <NavBarHeader setShowDropDown={setShowDropDown} showDropDown={showDropDown} />
                     <NavBarMain isMenuExpanded={isMenuExpanded} setIsMenuExpanded={setIsMenuExpanded} isAdmin={isAdmin} />
-                    <NavMenu isMenuExpanded={isMenuExpanded} setIsMenuExpanded={setIsMenuExpanded} navItems={getAdminNavItems(t)} />
+                    <NavMenu isMenuExpanded={isMenuExpanded} setIsMenuExpanded={setIsMenuExpanded} navItems={adminRoutes} />
                 </div>
             </header>
         </>
