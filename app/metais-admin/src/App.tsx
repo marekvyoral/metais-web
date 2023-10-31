5d0a9b864bf43b84078a07f90825fe302a2b1f64
Oleksandr Panyshchev <oleksandr.panyshchev@isdd.sk>
2023-10-31 15:44:07 +0000

    Bugfix/met 702/admin app name

--- app/metais-admin/src/App.tsx
+++ app/metais-admin/src/App.tsx
@@ -1,11 +1,13 @@
 import React, { Suspense } from 'react'
 import { useUserInfo } from '@isdd/metais-common/hooks/useUserInfo'
+import { useTranslation } from 'react-i18next'
 
 import { Router } from '@/navigation/Router'
 
 export const App: React.FC = () => {
     useUserInfo()
-
+    const { t } = useTranslation()
+    document.title = `${t('titles.mainPage')}`
     return (
         <Suspense>
             <Router />
