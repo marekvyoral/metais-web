049bd07e2a14b307374040f2925b801b4961e003
Matej Svec <matej.svec@dynatech.sk>
2023-11-08 14:18:10 +0100

    webPortalClient

--- packages/metais-common/src/contexts/auth/authContext.tsx
+++ packages/metais-common/src/contexts/auth/authContext.tsx
@@ -40,7 +40,7 @@ const baseUrl =
     import.meta.env.VITE_REST_CLIENT_IAM_OIDC_BASE_URL + (import.meta.env.VITE_IAM_OIDC_PATH ? `/${import.meta.env.VITE_IAM_OIDC_PATH}` : '')
 
 const authConfig: TAuthConfig = {
-    clientId: 'webPortalClient2',
+    clientId: 'webPortalClient',
     extraAuthParameters: { response_type: 'code' },
     authorizationEndpoint: baseUrl + '/authorize',
     tokenEndpoint: baseUrl + '/token',
