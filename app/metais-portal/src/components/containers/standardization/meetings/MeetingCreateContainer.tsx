20210218967a907c251671699ba333bb7026bc6f
Mariia Naduieva <mariia.naduieva@isdd.sk>
2023-11-19 17:29:48 +0000

    add files, linksImport

--- app/metais-portal/src/components/containers/standardization/meetings/MeetingCreateContainer.tsx
+++ app/metais-portal/src/components/containers/standardization/meetings/MeetingCreateContainer.tsx
@@ -2,7 +2,7 @@ import { NavigationSubRoutes } from '@isdd/metais-common/navigation/routeNames'
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import { FieldValues } from 'react-hook-form'
-import { useCreateVote1 } from '@isdd/metais-common/api/generated/standards-swagger'
+import { ApiAttachment, useCreateVote1 } from '@isdd/metais-common/api/generated/standards-swagger'
 import { useActionSuccess } from '@isdd/metais-common/contexts/actionSuccess/actionSuccessContext'
 
 import { MeetingCreateEditView } from '@/components/views/standardization/meetings/MeetingCreateEditView'
@@ -28,7 +28,7 @@ export const MeetingCreateContainer: React.FC = () => {
         },
     })
 
-    const onSubmit = (formData: FieldValues) => {
+    const onSubmit = (formData: FieldValues, attachments: ApiAttachment[]) => {
         createMeeting({
             data: {
                 name: formData[MeetingFormEnum.NAME],
@@ -42,6 +42,8 @@ export const MeetingCreateContainer: React.FC = () => {
                 ...(formData[MeetingFormEnum.MEETING_EXTERNAL_ACTORS]?.length > 0 && {
                     meetingExternalActors: formData[MeetingFormEnum.MEETING_EXTERNAL_ACTORS],
                 }),
+                meetingLinks: formData[MeetingFormEnum.MEETING_LINKS],
+                meetingAttachments: attachments,
             },
         })
     }
