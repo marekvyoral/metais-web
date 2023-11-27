17e893bc3de4f528d08eebbad5b8021731cc2ded
Peter Flimmel <peter.flimmel@isdd.sk>
2023-11-27 10:59:39 +0100

    update status = READY

--- packages/idsk-ui-kit/src/tabs/tabsUtils.ts
+++ packages/idsk-ui-kit/src/tabs/tabsUtils.ts
@@ -1,9 +1,9 @@
 import { Tab } from './Tabs'
 
-export const changeTabOrder = (element: Tab, index: number, newTabList: Tab[], setNewTabList: React.Dispatch<React.SetStateAction<Tab[]>>) => {
+export const changeTabOrder = (element: Tab, index: number, newTabList: Tab[], setNewTabList: React.Dispatch<React.SetStateAction<Tab[]>>): void => {
     const localTablist = [...newTabList]
     if (index < 0 || index >= localTablist.length) {
-        return localTablist
+        return
     }
     localTablist.splice(localTablist.indexOf(element), 1)
     localTablist.splice(index, 0, element)
