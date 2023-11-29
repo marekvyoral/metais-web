e8a710b8a04fde58ba02f7998fb2a61aa53b626d
Hryshchuk Mykola <mykola.hryshchuk@isdd.sk>
2023-11-29 14:42:51 +0100

    METT-612: add default data on create codelist

--- app/metais-portal/src/pages/data-objects/requestlist/[requestId]/edit/index.tsx
+++ app/metais-portal/src/pages/data-objects/requestlist/[requestId]/edit/index.tsx
@@ -2,29 +2,7 @@ import { EditRequestContainer } from '@/components/containers/EditRequestContain
 import { CreateRequestView } from '@/components/views/requestLists/CreateRequestView'
 
 const RequestListEditPage = () => {
-    return (
-        <EditRequestContainer
-            View={(props) => (
-                <CreateRequestView
-                    canEdit={props.canEdit}
-                    canEditDate={props.canEditDate}
-                    isError={props.isError}
-                    errorMessages={props.errorMessages}
-                    isLoading={props.isLoading}
-                    isLoadingMutation={props.isLoadingMutation}
-                    firstNotUsedCode={props.firstNotUsedCode}
-                    onHandleCheckIfCodeIsAvailable={props.onHandleCheckIfCodeIsAvailable}
-                    loadOptions={props.loadOptions}
-                    onSave={props.onSave}
-                    onSend={props.onSend}
-                    editData={props.editData}
-                    attributeProfile={props.attributeProfile}
-                    onSaveDates={props.onSaveDates}
-                    requestId={props.requestId}
-                />
-            )}
-        />
-    )
+    return <EditRequestContainer View={(props) => <CreateRequestView {...props} />} />
 }
 
 export default RequestListEditPage
