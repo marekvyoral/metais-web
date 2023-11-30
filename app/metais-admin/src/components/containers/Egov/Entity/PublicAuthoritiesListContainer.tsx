cd19eee2d35e7c159a1864b33634e0a189b5650c
Badura Filip <filip.badura@isdd.sk>
2023-11-30 15:30:03 +0000

    Fix/mett 628

--- app/metais-admin/src/components/containers/Egov/Entity/PublicAuthoritiesListContainer.tsx
+++ app/metais-admin/src/components/containers/Egov/Entity/PublicAuthoritiesListContainer.tsx
@@ -4,8 +4,12 @@ import { mapFilterParamsToApi } from '@isdd/metais-common/componentHelpers/filte
 import { useUserPreferences } from '@isdd/metais-common/contexts/userPreferences/userPreferencesContext'
 import { IFilterParams } from '@isdd/metais-common/hooks/useFilter'
 import { IListView } from '@isdd/metais-common/types/list'
-import React from 'react'
+import React, { Dispatch, SetStateAction, useEffect, useState } from 'react'
 import { FieldValues } from 'react-hook-form'
+import { useRedirectAfterSuccess } from '@isdd/metais-common/hooks/useRedirectAfterSucces'
+import { useIsIdentityAssignedToPOHook } from '@isdd/metais-common/api/generated/iam-swagger'
+import { useActionSuccess } from '@isdd/metais-common/contexts/actionSuccess/actionSuccessContext'
+import { AdminRouteNames } from '@isdd/metais-common/navigation/routeNames'
 export interface IActions {
     setInvalid?: (entityId: string | undefined, configurationItem: ConfigurationItemUiAttributes | undefined) => Promise<void>
 }
@@ -15,6 +19,7 @@ interface IPublicAuthoritiesListContainer<T> {
     ListComponent: React.FC<IListView & IActions>
     defaultFilterValues: T
     entityId?: string
+    setIsInvalidateError: Dispatch<SetStateAction<boolean>>
 }
 
 export const PublicAuthoritiesListContainer = <T extends FieldValues & IFilterParams>({
@@ -22,6 +27,7 @@ export const PublicAuthoritiesListContainer = <T extends FieldValues & IFilterPa
     ListComponent,
     defaultFilterValues,
     entityId,
+    setIsInvalidateError,
 }: IPublicAuthoritiesListContainer<T>) => {
     const { columnListData, saveColumnSelection, resetColumns, isLoading: isColumnsLoading, isError: isColumnsError } = useGetColumnData(entityName)
     const { currentPreferences } = useUserPreferences()
@@ -56,22 +62,66 @@ export const PublicAuthoritiesListContainer = <T extends FieldValues & IFilterPa
 
     const pagination = usePagination(tableData, filterParams)
 
-    const { mutateAsync: setInvalid } = useInvalidateConfigurationItem()
+    const { mutateAsync: setInvalid, isLoading: isInvalidating, isError: isInvalidateError } = useInvalidateConfigurationItem()
+    const isAssignToPOHook = useIsIdentityAssignedToPOHook()
 
-    const InvalidateConfigurationItem = async (uuid: string | undefined, configurationItem: ConfigurationItemUiAttributes | undefined) => {
-        if (!configurationItem) return
-        await setInvalid({
-            data: {
-                attributes: Object.keys(configurationItem).map((key) => ({ value: configurationItem?.[key], name: key })),
-                invalidateReason: { comment: '' },
-                type: 'PO',
-                uuid: uuid,
-            },
-        })
+    const [requestId, setRequestId] = useState<string | null>(null)
+    const [isCheckError, setIsCheckError] = useState(false)
+
+    const { setIsActionSuccess } = useActionSuccess()
+
+    const onCreateSuccess = () => {
+        setRequestId(null)
+        refetch()
+        setIsActionSuccess({ value: true, path: `${AdminRouteNames.PUBLIC_AUTHORITIES_LIST}` })
     }
 
-    const isLoading = [isReadCiListLoading, isColumnsLoading].some((item) => item)
-    const isError = [isReadCiListError, isColumnsError].some((item) => item)
+    const {
+        performRedirection,
+        reset: resetRedirect,
+        isLoading: isRedirectLoading,
+        isError: isRedirectError,
+        isFetched: isRedirectFetched,
+        isProcessedError,
+        isTooManyFetchesError,
+    } = useRedirectAfterSuccess({
+        requestId: requestId ?? '',
+        onSuccess: onCreateSuccess,
+    })
+
+    useEffect(() => {
+        setIsInvalidateError([isCheckError, isProcessedError, isInvalidateError].some((item) => item))
+    }, [isCheckError, isInvalidateError, isProcessedError, setIsInvalidateError])
+
+    const invalidateConfigurationItem = async (uuid: string | undefined, configurationItem: ConfigurationItemUiAttributes | undefined) => {
+        if (!configurationItem) return
+        resetRedirect()
+        await isAssignToPOHook(uuid ?? '')
+            .then((res) => {
+                if (!res) {
+                    setInvalid({
+                        data: {
+                            attributes: Object.keys(configurationItem).map((key) => ({ value: configurationItem?.[key], name: key })),
+                            invalidateReason: { comment: '' },
+                            type: 'PO',
+                            uuid: uuid,
+                        },
+                    }).then((invalidRes) => setRequestId(invalidRes.requestId ?? ''))
+                }
+            })
+            .catch(() => {
+                setIsCheckError(true)
+            })
+    }
+
+    useEffect(() => {
+        if (requestId != null) {
+            performRedirection()
+        }
+    }, [performRedirection, requestId])
+
+    const isLoading = [isReadCiListLoading, isColumnsLoading, isRedirectFetched && isRedirectLoading].some((item) => item)
+    const isError = [isReadCiListError, isColumnsError, isProcessedError, isRedirectError, isTooManyFetchesError].some((item) => item)
 
     return (
         <ListComponent
@@ -82,9 +132,9 @@ export const PublicAuthoritiesListContainer = <T extends FieldValues & IFilterPa
             storeUserSelectedColumns={saveColumnSelection}
             sort={filterParams?.sort ?? []}
             refetch={refetch}
-            isLoading={isLoading}
+            isLoading={isLoading || isInvalidating}
             isError={isError}
-            setInvalid={InvalidateConfigurationItem}
+            setInvalid={invalidateConfigurationItem}
         />
     )
 }
--- app/metais-admin/src/pages/public-authorities/list.tsx
+++ app/metais-admin/src/pages/public-authorities/list.tsx
@@ -1,9 +1,12 @@
 import { BreadCrumbs, HomeIcon, TextHeading } from '@isdd/idsk-ui-kit/index'
 import { FlexColumnReverseWrapper } from '@isdd/metais-common/components/flex-column-reverse-wrapper/FlexColumnReverseWrapper'
 import { IFilterParams } from '@isdd/metais-common/hooks/useFilter'
-import { QueryFeedback } from '@isdd/metais-common/index'
+import { MutationFeedback, QueryFeedback } from '@isdd/metais-common/index'
 import { AdminRouteNames } from '@isdd/metais-common/navigation/routeNames'
 import { useTranslation } from 'react-i18next'
+import { useScroll } from '@isdd/metais-common/hooks/useScroll'
+import { useActionSuccess } from '@isdd/metais-common/contexts/actionSuccess/actionSuccessContext'
+import { useEffect, useState } from 'react'
 
 import { MainContentWrapper } from '@/components/MainContentWrapper'
 import { PublicAuthoritiesListContainer } from '@/components/containers/Egov/Entity/PublicAuthoritiesListContainer'
@@ -21,6 +24,15 @@ const PublicAuthoritiesPage = () => {
     const entityName = 'PO'
     const defaultFilterValues: PublicAuthoritiesFilterData = { Gen_Profil_nazov: '', EA_Profil_PO_typ_osoby: [], EA_Profil_PO_ico: '' }
     const { t } = useTranslation()
+    const { wrapperRef: mutationRef, scrollToMutationFeedback: mutationScroll } = useScroll()
+    const { isActionSuccess } = useActionSuccess()
+    const [isInvalidateError, setIsInvalidateError] = useState(false)
+    useEffect(() => {
+        if (isActionSuccess.value || isInvalidateError) {
+            mutationScroll()
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [isActionSuccess.value, isInvalidateError])
     return (
         <>
             <BreadCrumbs
@@ -33,6 +45,7 @@ const PublicAuthoritiesPage = () => {
             <PublicAuthoritiesListContainer<PublicAuthoritiesFilterData>
                 entityName={entityName}
                 defaultFilterValues={defaultFilterValues}
+                setIsInvalidateError={setIsInvalidateError}
                 ListComponent={({
                     data: { tableData },
                     handleFilterChange,
@@ -43,28 +56,39 @@ const PublicAuthoritiesPage = () => {
                     isError,
                     isLoading,
                     setInvalid,
-                }) => (
-                    <MainContentWrapper>
-                        <QueryFeedback withChildren loading={isLoading} error={false}>
-                            <FlexColumnReverseWrapper>
-                                <TextHeading size="XL">{t('navMenu.publicAuthorities.publicAuthorities')}</TextHeading>
-                                {isError && <QueryFeedback error={isError} loading={false} />}
-                            </FlexColumnReverseWrapper>
-                            <OrganizationFilter defaultFilterValues={defaultFilterValues} />
-                            <PublicAuthoritiesTable
-                                data={tableData}
-                                handleFilterChange={handleFilterChange}
-                                storeUserSelectedColumns={storeUserSelectedColumns}
-                                resetUserSelectedColumns={resetUserSelectedColumns}
-                                pagination={pagination}
-                                sort={sort}
-                                isLoading={isLoading}
-                                error={isError}
-                                setInvalid={setInvalid}
-                            />
-                        </QueryFeedback>
-                    </MainContentWrapper>
-                )}
+                }) => {
+                    return (
+                        <MainContentWrapper>
+                            <QueryFeedback withChildren loading={isLoading} error={false}>
+                                <FlexColumnReverseWrapper>
+                                    <TextHeading size="XL">{t('navMenu.publicAuthorities.publicAuthorities')}</TextHeading>
+                                    {(isInvalidateError || isActionSuccess.value) && (
+                                        <div ref={mutationRef}>
+                                            <MutationFeedback
+                                                error={!isActionSuccess.value ? t('mutationFeedback.invalidatePOError') : ''}
+                                                success={isActionSuccess.value}
+                                                successMessage={t('mutationFeedback.invalidatePOSuccess')}
+                                            />
+                                        </div>
+                                    )}
+                                    {isError && <QueryFeedback error={isError} loading={false} />}
+                                </FlexColumnReverseWrapper>
+                                <OrganizationFilter defaultFilterValues={defaultFilterValues} />
+                                <PublicAuthoritiesTable
+                                    data={tableData}
+                                    handleFilterChange={handleFilterChange}
+                                    storeUserSelectedColumns={storeUserSelectedColumns}
+                                    resetUserSelectedColumns={resetUserSelectedColumns}
+                                    pagination={pagination}
+                                    sort={sort}
+                                    isLoading={isLoading}
+                                    error={isError}
+                                    setInvalid={setInvalid}
+                                />
+                            </QueryFeedback>
+                        </MainContentWrapper>
+                    )
+                }}
             />
         </>
     )
