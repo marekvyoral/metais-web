b682e8e1409ae947573561dd8832b58c8952c41b
Hryshchuk Mykola <mykola.hryshchuk@isdd.sk>
2023-12-27 15:08:38 +0100

    METT-712: fix vote states

--- app/metais-portal/src/components/views/standardization/votes/votesList/VoteListView.tsx
+++ app/metais-portal/src/components/views/standardization/votes/votesList/VoteListView.tsx
@@ -2,23 +2,28 @@ import { Button, Filter, Input, PaginatorWrapper, SimpleSelect, Table, TextHeadi
 import { IFilter } from '@isdd/idsk-ui-kit/types'
 import { BASE_PAGE_NUMBER, BASE_PAGE_SIZE } from '@isdd/metais-common/api/constants'
 import { ApiError, ApiVotePreviewList } from '@isdd/metais-common/api/generated/standards-swagger'
+import { DEFAULT_PAGESIZE_OPTIONS } from '@isdd/metais-common/constants'
 import { useActionSuccess } from '@isdd/metais-common/contexts/actionSuccess/actionSuccessContext'
+import { useAbilityContext } from '@isdd/metais-common/hooks/permissions/useAbilityContext'
+import { Actions } from '@isdd/metais-common/hooks/permissions/useVotesListPermissions'
 import { IFilterParams } from '@isdd/metais-common/hooks/useFilter'
+import { useScroll } from '@isdd/metais-common/hooks/useScroll'
 import { ActionsOverTable, MutationFeedback, QueryFeedback } from '@isdd/metais-common/index'
 import { NavigationSubRoutes } from '@isdd/metais-common/navigation/routeNames'
+import { QueryObserverResult, RefetchOptions, RefetchQueryFilters } from '@tanstack/react-query'
+import { useEffect } from 'react'
 import { useTranslation } from 'react-i18next'
 import { useLocation, useNavigate } from 'react-router-dom'
-import { DEFAULT_PAGESIZE_OPTIONS } from '@isdd/metais-common/constants'
-import { useScroll } from '@isdd/metais-common/hooks/useScroll'
-import { useEffect } from 'react'
-import { QueryObserverResult, RefetchOptions, RefetchQueryFilters } from '@tanstack/react-query'
-import { Actions } from '@isdd/metais-common/hooks/permissions/useVotesListPermissions'
-import { useAbilityContext } from '@isdd/metais-common/hooks/permissions/useAbilityContext'
 
 import styles from './voteList.module.scss'
 
-import { getVoteStateExplanation } from '@/components/views/standardization/votes/voteProps'
-import { voteListColumns, voteStateOptions, votesTypeToShowOptions } from '@/components/views/standardization/votes/votesList/voteListProps'
+import { VoteStateOptionEnum, getVoteStateExplanation } from '@/components/views/standardization/votes/voteProps'
+import {
+    voteListColumns,
+    voteStateOptions,
+    voteStateWithoutDate,
+    votesTypeToShowOptions,
+} from '@/components/views/standardization/votes/votesList/voteListProps'
 
 export interface IVotesListFilterData extends IFilterParams, IFilter {
     votesTypeToShow: string
@@ -91,39 +96,48 @@ export const VotesListView: React.FC<IVotesListView> = ({
             <Filter<IVotesListFilterData>
                 heading={t('votes.votesList.filter.title')}
                 defaultFilterValues={defaultFilterValues}
-                form={({ filter: listFilter, register, setValue }) => (
-                    <div>
-                        {isUserLogged && (
+                form={({ filter: listFilter, register, setValue, watch }) => {
+                    const voteState = watch('voteState')
+                    return (
+                        <div>
+                            {isUserLogged && (
+                                <SimpleSelect
+                                    id="votesTypeToShow"
+                                    label={t('votes.votesList.filter.votesTypeToShow')}
+                                    options={votesTypeToShowOptions(t)}
+                                    setValue={setValue}
+                                    defaultValue={listFilter?.votesTypeToShow}
+                                    name="votesTypeToShow"
+                                />
+                            )}
                             <SimpleSelect
-                                id="votesTypeToShow"
-                                label={t('votes.votesList.filter.votesTypeToShow')}
-                                options={votesTypeToShowOptions(t)}
+                                id="voteState"
+                                label={t('votes.votesList.filter.voteState')}
+                                options={voteStateOptions(t)}
                                 setValue={setValue}
-                                defaultValue={listFilter?.votesTypeToShow}
-                                name="votesTypeToShow"
+                                defaultValue={listFilter?.voteState}
+                                name="voteState"
                             />
-                        )}
-                        <SimpleSelect
-                            id="voteState"
-                            label={t('votes.votesList.filter.voteState')}
-                            options={voteStateOptions(t)}
-                            setValue={setValue}
-                            defaultValue={listFilter?.voteState}
-                            name="voteState"
-                        />
-
-                        <div className={styles.inline}>
-                            <Input
-                                {...register('effectiveFrom')}
-                                type="date"
-                                label={t('votes.votesList.filter.fromDate')}
-                                className={styles.stretch}
-                            />
-                            <div className={styles.space} />
-                            <Input {...register('effectiveTo')} type="date" label={t('votes.votesList.filter.toDate')} className={styles.stretch} />
+                            {!voteStateWithoutDate.includes(voteState as VoteStateOptionEnum) && (
+                                <div className={styles.inline}>
+                                    <Input
+                                        {...register('effectiveFrom')}
+                                        type="date"
+                                        label={t('votes.votesList.filter.fromDate')}
+                                        className={styles.stretch}
+                                    />
+                                    <div className={styles.space} />
+                                    <Input
+                                        {...register('effectiveTo')}
+                                        type="date"
+                                        label={t('votes.votesList.filter.toDate')}
+                                        className={styles.stretch}
+                                    />
+                                </div>
+                            )}
                         </div>
-                    </div>
-                )}
+                    )
+                }}
             />
             <div className={styles.inline}>
                 {ability.can(Actions.CREATE, 'VOTE') ? (
