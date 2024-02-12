4612cc5c793ca9f6bcb79631312f215bdc71a07e
Svec Matej <matej.svec@isdd.sk>
2024-02-12 10:24:01 +0000

    Feature/relationships search

--- app/metais-portal/src/components/navbar/navigationItems.ts
+++ app/metais-portal/src/components/navbar/navigationItems.ts
@@ -72,6 +72,7 @@ const getEgovSubItems = (t: TFunction, isSideMenu?: boolean) => {
         { title: t('navMenu.lists.webResidence'), path: NavigationSubRoutes.WEBOVE_SIDLO },
         { title: t('navMenu.lists.training'), path: NavigationSubRoutes.TRAINING },
         { title: t('navMenu.lists.personalProcedures'), path: NavigationSubRoutes.OSOBITNY_POSTUP },
+        { title: t('navMenu.lists.relationsSearch'), path: NavigationSubRoutes.RELATIONS_LIST },
         //{ title: t('navMenu.lists.educationalCourses'), path: NavigationSubRoutes.EDUCATIONAL_COURSES },
         //{ title: t('navMenu.lists.evidenceObjects'), path: NavigationSubRoutes.OBJEKTY_EVIDENCIE },
         //{ title: t('navMenu.lists.attributesEvidenceObjects'), path: NavigationSubRoutes.ATRIBUTY_OBJEKTY_EVIDENCIE },
