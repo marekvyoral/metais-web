c4be751d86365fccbd949c251b535a98636280d9
Peter Flimmel <peter.flimmel@isdd.sk>
2024-02-20 14:43:18 +0100

    Category ENUM cleanup

--- packages/metais-common/src/permissions/ci/index.ts
+++ packages/metais-common/src/permissions/ci/index.ts
@@ -1,18 +1,15 @@
 import { Group, User } from '@isdd/metais-common/contexts/auth/authContext'
 import { CiType } from '@isdd/metais-common/api/generated/types-repo-swagger'
-
-enum CATEGORY_ENUM {
-    NOT_VISIBLE = 'NO',
-    READ_ONLY = 'ROCB',
-    READ_WRITE = 'RWCB',
-}
+import { CATEGORY_ENUM } from '@isdd/metais-common/api/constants'
 
 enum TYPES {
     SYSTEM = 'system',
 }
 
 export const isCiTypeUpdatable = (ciType?: CiType) => {
-    return ciType && ciType.category === CATEGORY_ENUM.READ_WRITE && ciType.type !== TYPES.SYSTEM
+    return (
+        ciType && (ciType.category === CATEGORY_ENUM.READ_WRITE || ciType.category === CATEGORY_ENUM.READ_WRITE_BDA) && ciType.type !== TYPES.SYSTEM
+    )
 }
 
 export const canCreateCiForType = (ciType?: CiType) => {
