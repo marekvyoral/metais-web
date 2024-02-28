95c55d104140bb6f037dc971bb46d031fd467518
Mariia Naduieva <mariia.naduieva@isdd.sk>
2024-02-28 11:36:22 +0000

    fix filter group

--- app/metais-portal/src/components/views/standardization/groups/components/GroupSelect.tsx
+++ app/metais-portal/src/components/views/standardization/groups/components/GroupSelect.tsx
@@ -1,66 +1,43 @@
-import React, { SetStateAction, useCallback } from 'react'
-import { SelectLazyLoading } from '@isdd/idsk-ui-kit/index'
-import { Find2111Params, Group, useFind2111Hook } from '@isdd/metais-common/api/generated/iam-swagger'
-import { OptionProps, components } from 'react-select'
+import React, { SetStateAction, useMemo } from 'react'
+import { IOption, SimpleSelect } from '@isdd/idsk-ui-kit/index'
+import { useFind2111 } from '@isdd/metais-common/api/generated/iam-swagger'
 import { useTranslation } from 'react-i18next'
-
-import styles from '@/components/views/standardization/groups/groupslist.module.scss'
+import { QueryFeedback } from '@isdd/metais-common/index'
 
 interface IGroupSelect {
-    selectedGroup: Group | undefined
-    setSelectedGroup: React.Dispatch<SetStateAction<Group | undefined>>
+    setSelectedGroup: React.Dispatch<SetStateAction<string>>
 }
 
-export const GroupSelect: React.FC<IGroupSelect> = ({ selectedGroup, setSelectedGroup }) => {
+export const GroupSelect: React.FC<IGroupSelect> = ({ setSelectedGroup }) => {
     const { t } = useTranslation()
 
-    const groupOptionsHook = useFind2111Hook()
+    const { data: groupList, isLoading } = useFind2111({ sortBy: 'name', ascending: false })
 
-    const loadGroupOptions = useCallback(
-        async (searchQuery: string, additional: { page: number } | undefined) => {
-            const page = !additional?.page ? 1 : (additional?.page || 0) + 1
-            const request: Find2111Params = {
-                sortBy: 'name',
-                ascending: false,
-            }
-            if (searchQuery && searchQuery.length > 0) request.name = searchQuery
-
-            const groupOptions = await groupOptionsHook(request)
-
-            return {
-                options: (groupOptions as Group[]) || [],
-                hasMore: false,
-                additional: {
-                    page,
+    const optionsGroups: IOption<string>[] = useMemo(() => {
+        if (Array.isArray(groupList)) {
+            return groupList?.map((group) => ({
+                value: `${group.shortName}`,
+                label: `${group.name} (${group.shortName})`,
+            }))
+        } else if (groupList) {
+            return [
+                {
+                    value: `${groupList.shortName}`,
+                    label: `${groupList.name} (${groupList.shortName})`,
                 },
-            }
-        },
-        [groupOptionsHook],
-    )
-
-    const selectLazyLoadingGroupOption = (props: OptionProps<Group>) => {
-        return (
-            <components.Option {...props} className={styles.selectOption}>
-                <div className={styles.selectOption}>
-                    <span>{props.data.name}</span>
-                    <span>
-                        <small> ({props.data.shortName})</small>
-                    </span>
-                </div>
-            </components.Option>
-        )
-    }
+            ]
+        }
+        return []
+    }, [groupList])
 
     return (
-        <SelectLazyLoading<Group>
-            name="account"
-            label={`${t('groups.groupName')}`}
-            value={selectedGroup}
-            onChange={(val) => setSelectedGroup(Array.isArray(val) ? val[0] : val)}
-            getOptionValue={(item) => item.uuid || ''}
-            getOptionLabel={(item) => item.name || ''}
-            option={(props) => selectLazyLoadingGroupOption(props)}
-            loadOptions={(searchTerm, _, additional) => loadGroupOptions(searchTerm, additional)}
-        />
+        <QueryFeedback loading={isLoading}>
+            <SimpleSelect
+                name="account"
+                label={`${t('groups.groupName')}`}
+                onChange={(val) => setSelectedGroup(Array.isArray(val) ? val[0] : val)}
+                options={optionsGroups}
+            />
+        </QueryFeedback>
     )
 }
