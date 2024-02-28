ccbcb272afb79ebbeff21a66b6eb2b7c70952e5b
Mariia Naduieva <mariia.naduieva@isdd.sk>
2024-02-28 13:02:26 +0000

    FIX URL

--- packages/metais-common/src/api/fetchCanCreateGraph.ts
+++ packages/metais-common/src/api/fetchCanCreateGraph.ts
@@ -1,4 +1,4 @@
-const baseURL = import.meta.env.VITE_REST_CLIENT_BASE_URL
+const baseURL = import.meta.env.VITE_API_BASE_URL
 
 export const fetchCanCreateGraph = async (accessToken: string) => {
     const response = await fetch(`${baseURL}/controls/checkUserRights/canCreateGraph`, {
